c2d1b347034fb3a90bd166d002bd408857a5f668
buddhamagnet <buddhamagnet@gmail.com>
2018-11-13 18:26:52 +0000

    better test

--- test/checks.js
+++ test/checks.js
@@ -1,12 +1,11 @@
 const  assert = require('assert');
-const checks = require('../checks');
-console.dir(checks);
+const { checkTransform }  = require('../checks');
 console.log(typeof checks)
 describe('Checks', function() {
   describe('transform', function() {
     const template = require('../samples/invalid/template-invalid-transform.json');
     it('returns an error given an invalid transform', function() {
-        const result = checks[0](template);
+        const result = checkTransform(template);
         assert.equal('Transform header does not equal AWS::Serverless-2016-10-31', result.message);
     });
   });
